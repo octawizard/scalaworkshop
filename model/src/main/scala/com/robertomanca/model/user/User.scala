a5cad794f6ed56281b6aac229871ade1068d943d
Roberto Manca <roberto.manca@edreamsodigeo.com>
2017-04-07 13:13:29 +0200

    cosmetic

--- model/src/main/scala/com/robertomanca/model/user/User.scala
+++ model/src/main/scala/com/robertomanca/model/user/User.scala
@@ -7,11 +7,15 @@ import com.robertomanca.model.location.Location
   * Created by Roberto Manca (roberto.manca@edreamsodigeo.com) on 24/03/2017.
   */
 abstract class User extends PrintableUser
+
 case class AppUser(var id: Long, var email: String, var name: String, var surname: String, var password: String, var favouriteLocations: List[Location]) extends User {
+
   override def toString =
     "User(id=" + id + ", email=" + email + ", name=" + name + ", surname=" + surname +")";
 }
+
 case class CorporateUser(var id: Long, var email: String, var name: String, var surname: String, var password: String, var companyId: Long, var favouriteLocations: List[Location]) extends User {
+
   override def toString =
     "User(id=" + id + ", email=" + email + ", name=" + name + ", surname=" + surname +", companyId=" + companyId + ")";
 }
