8fd6fdc8a275a46061276e98fee1e8261cb58e23
Roberto Manca <roberto.manca@edreamsodigeo.com>
2017-12-28 12:04:49 +0100

    prepared exercise 3

--- repository/src/main/scala/com/robertomanca/repository/UserRepository.scala
+++ repository/src/main/scala/com/robertomanca/repository/UserRepository.scala
@@ -1,15 +1,15 @@
 package com.robertomanca.repository
 
-import com.robertomanca.model.location.Location
-import com.robertomanca.model.user.{AppUser, CorporateUser, User}
+import com.robertomanca.model.user.User
 import com.robertomanca.repository.contract.UserRepositoryTrait
 
-import scala.annotation.tailrec
 import scala.collection.mutable.ListBuffer
 
 /**
   * Created by Roberto Manca (roberto.manca@edreamsodigeo.com) on 07/04/2017.
   */
 class UserRepository(var users: ListBuffer[User]) extends UserRepositoryTrait {
+  override def getFavouriteLocations = ???
 
+  override def getFavouriteLocationsByCountry(country: String) = ???
 }
