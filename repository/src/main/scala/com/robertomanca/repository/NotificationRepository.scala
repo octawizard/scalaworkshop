8943050509b5b802709334d0fcdfe815a6a3adbd
Roberto Manca <roberto.manca@edreamsodigeo.com>
2017-12-28 12:05:54 +0100

    prepared exercise 4

--- repository/src/main/scala/com/robertomanca/repository/NotificationRepository.scala
+++ repository/src/main/scala/com/robertomanca/repository/NotificationRepository.scala
@@ -9,4 +9,5 @@ import com.robertomanca.repository.contract.NotificationRepositoryTrait
   */
 class NotificationRepository extends NotificationRepositoryTrait{
 
+  override def get(params: Any) = ???
 }
