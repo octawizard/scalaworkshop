396b3b6243b1db4c66c5606f9906f3e88f20de1a
Roberto Manca <roberto.manca@edreamsodigeo.com>
2017-12-28 11:05:16 +0100

    prepared exercise 5

--- model/src/main/scala/com/robertomanca/model/functions/Functions.scala
+++ model/src/main/scala/com/robertomanca/model/functions/Functions.scala
@@ -11,35 +11,13 @@ import com.robertomanca.model.user.User
   */
 object Functions extends LocationsTrait with App with BookingsTrait {
 
-  val anonFlightSummaryFunc = (flight: Flight) => new FlightSummary(flight.inbound.origin, flight.inbound.destination, flight.outbound.origin, flight.outbound.destination)
+  val flightSummaryFunc: Function1[Flight, FlightSummary] = ???
 
-  val flightSummaryFunc = new Function1[Flight, FlightSummary] {
-    override def apply(f: Flight) = new FlightSummary(f.inbound.origin, f.inbound.destination, f.outbound.origin, f.outbound.destination)
-  }
+  val bookingSummaryFunc: Function2[Flight, User, BookingSummary] = ???
 
-  val anonBookingSummaryFunc = (flight: Flight, user: User) =>
-    new BookingSummary(
-      new FlightSummary(flight.inbound.origin, flight.inbound.destination, flight.outbound.origin, flight.outbound.destination),
-      user.name, user.surname
-    )
+  /* curried params method */
+  def bookedFlightsFromTo(outboundOrigin: Location)(outboundDestination: Location)(bookings: List[Booking]) = ???
 
-  val bookingSummaryFunc = new Function2[Flight, User, BookingSummary] {
-    override def apply(f: Flight, u: User) = new BookingSummary(
-      new FlightSummary(f.inbound.origin, f.inbound.destination, f.outbound.origin, f.outbound.destination),
-      u.name, u.surname
-    )
-  }
-
-  def bookedFlightsFromTo(outboundOrigin: Location)(outboundDestination: Location)(bookings: List[Booking]) = // curried params method
-    bookings.flatMap(booking => booking.flights)
-      .filter(flight => flight.outbound.origin == outboundOrigin && flight.outbound.destination == outboundDestination)
-
-  val bookedFlightsFromRome = bookedFlightsFromTo(rome) _ // partially applied function of the curried params method
-
-  val allBookings = List(booking1, booking2, booking3, booking4)
-
-  assert(bookedFlightsFromRome(barcelona)(allBookings) == bookedFlightsFromRome.apply(barcelona).apply(allBookings))
-  println(bookedFlightsFromRome(barcelona)(allBookings))
-  println(bookedFlightsFromRome.apply(barcelona).apply(allBookings))
+  val bookedFlightsFromRome = ???
 
 }
